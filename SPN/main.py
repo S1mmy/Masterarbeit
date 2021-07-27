1fdfc5adf412cd297a3a8591e05324bc00d446d3
Sammy <marvin.falkenhahn@informatik.hs-fulda.de>
2021-07-27 11:32:35 +0200

    Update

--- SPN/main.py
+++ SPN/main.py
@@ -1,111 +1,136 @@
+# Source: https://github.com/pronobis/libspn-keras/blob/master/examples/notebooks/Sampling%20with%20conv%20SPNs.ipynb
 import libspn_keras as spnk
 from tensorflow import keras
-import matplotlib.pyplot as plt
-
-%matplotlib inline
-
-# Model Data
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-# Make sure images have shape (28, 28, 1)
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
-print("x_train shape:", x_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
-
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-
-spnk.set_default_sum_op(spnk.SumOpGradBackprop())
 
 spnk.set_default_accumulator_initializer(
-    keras.initializers.TruncatedNormal(stddev=0.5, mean=1.0)
+    spnk.initializers.Dirichlet()
 )
 
-sum_product_network = keras.Sequential([
-  spnk.layers.NormalizeStandardScore(input_shape=(28, 28, 1)),
-  spnk.layers.NormalLeaf(
-      num_components=16, 
-      location_trainable=True,
-      location_initializer=keras.initializers.TruncatedNormal(
-          stddev=1.0, mean=0.0)
-  ),
-  # Non-overlapping products
-  spnk.layers.Conv2DProduct(
-      depthwise=True, 
-      strides=[2, 2], 
-      dilations=[1, 1], 
-      kernel_size=[2, 2],
-      padding='valid'
-  ),
-  spnk.layers.Local2DSum(num_sums=16),
-  # Non-overlapping products
-  spnk.layers.Conv2DProduct(
-      depthwise=True, 
-      strides=[2, 2], 
-      dilations=[1, 1], 
-      kernel_size=[2, 2],
-      padding='valid'
-  ),
-  spnk.layers.Local2DSum(num_sums=32),
-  # Overlapping products, starting at dilations [1, 1]
-  spnk.layers.Conv2DProduct(
-      depthwise=True, 
-      strides=[1, 1], 
-      dilations=[1, 1], 
-      kernel_size=[2, 2],
-      padding='full'
-  ),
-  spnk.layers.Local2DSum(num_sums=32),
-  # Overlapping products, with dilations [2, 2] and full padding
-  spnk.layers.Conv2DProduct(
-      depthwise=True, 
-      strides=[1, 1], 
-      dilations=[2, 2], 
-      kernel_size=[2, 2],
-      padding='full'
-  ),
-  spnk.layers.Local2DSum(num_sums=64),
-  # Overlapping products, with dilations [2, 2] and full padding
-  spnk.layers.Conv2DProduct(
-      depthwise=True, 
-      strides=[1, 1], 
-      dilations=[4, 4], 
-      kernel_size=[2, 2],
-      padding='full'
-  ),
-  spnk.layers.Local2DSum(num_sums=64),
-  # Overlapping products, with dilations [2, 2] and 'final' padding to combine 
-  # all scopes
-  spnk.layers.Conv2DProduct(
-      depthwise=True, 
-      strides=[1, 1], 
-      dilations=[8, 8], 
-      kernel_size=[2, 2],
-      padding='final'
-  ),
-  spnk.layers.SpatialToRegions(),
-  # Class roots
-  spnk.layers.DenseSum(num_sums=10),
-  spnk.layers.RootSum(return_weighted_child_logits=True)
-])
+import numpy as np
+import tensorflow_datasets as tfds
+from libspn_keras.layers import NormalizeAxes
+import tensorflow as tf
 
+def take_first(a, b):
+  return tf.reshape(tf.cast(a, tf.float32), (-1, 28, 28, 1))
 
-
-completion_model = define_spn(
-    sum_op=spnk.SumOpGradBackprop(logspace_accumulators=False)
+normalize = spnk.layers.NormalizeStandardScore(
+    input_shape=(28, 28, 1), axes=NormalizeAxes.GLOBAL, 
+    normalization_epsilon=1e-3
 )
-completion_model.set_weights(sum_product_network.get_weights())
-completion_model.compile(metrics=[keras.metrics.MeanSquaredError()])
 
-completion_model.evaluate([y_test], y_test, verbose=2)
-completion_out = model.predict([y_test])
-print(completion_out.shape)
+mnist_images = tfds.load(name="mnist", batch_size=32, split="train", as_supervised=True).map(take_first)
+normalize.adapt(mnist_images) 
+mnist_normalized = mnist_images.map(normalize)
+location_initializer = spnk.initializers.PoonDomingosMeanOfQuantileSplit(
+    mnist_normalized
+)
 
+def build_spn(sum_op, return_logits, infer_no_evidence=False):
+  spnk.set_default_sum_op(sum_op)
+  return spnk.models.SequentialSumProductNetwork([
+    normalize,
+    spnk.layers.NormalLeaf(
+        num_components=4, 
+        location_trainable=True,
+        location_initializer=location_initializer,
+        scale_trainable=True
+    ),
+    spnk.layers.Conv2DProduct(
+        depthwise=False, 
+        strides=[2, 2], 
+        dilations=[1, 1], 
+        kernel_size=[2, 2],
+        padding='valid'
+    ),
+    spnk.layers.Local2DSum(num_sums=256),
+    spnk.layers.Conv2DProduct(
+        depthwise=True, 
+        strides=[2, 2], 
+        dilations=[1, 1], 
+        kernel_size=[2, 2],
+        padding='valid'
+    ),
+    spnk.layers.Local2DSum(num_sums=512),
+    # Pad to go from 7x7 to 8x8, so that we can apply 3 more Conv2DProducts
+    tf.keras.layers.ZeroPadding2D(((0, 1), (0, 1))),
+    spnk.layers.Conv2DProduct(
+        depthwise=True, 
+        strides=[2, 2], 
+        dilations=[1, 1], 
+        kernel_size=[2, 2],
+        padding='valid'
+    ),
+    spnk.layers.Local2DSum(num_sums=512),
+    spnk.layers.Conv2DProduct(
+        depthwise=True, 
+        strides=[2, 2], 
+        dilations=[1, 1], 
+        kernel_size=[2, 2],
+        padding='valid'
+    ),
+    spnk.layers.Local2DSum(num_sums=1024),
+    spnk.layers.Conv2DProduct(
+        depthwise=True, 
+        strides=[2, 2], 
+        dilations=[1, 1], 
+        kernel_size=[2, 2],
+        padding='valid'
+    ),
+    spnk.layers.LogDropout(rate=0.5),
+    spnk.layers.DenseSum(num_sums=10),
+    spnk.layers.RootSum(return_weighted_child_logits=return_logits)
+  ], infer_no_evidence=infer_no_evidence, unsupervised=False)
+  
+sum_product_network = build_spn(spnk.SumOpEMBackprop(), return_logits=True)
+sum_product_network.summary()
+
+import tensorflow_datasets as tfds
+
+batch_size = 128
+
+mnist_train = (
+    tfds.load(name="mnist", split="train", as_supervised=True)
+    .shuffle(1024)
+    .batch(batch_size)
+)
+
+mnist_test = (
+    tfds.load(name="mnist", split="test", as_supervised=True)
+    .batch(100)
+)
+
+optimizer = spnk.optimizers.OnlineExpectationMaximization(learning_rate=0.05, accumulate_batches=1)
+metrics = []
+loss = spnk.losses.NegativeLogJoint()
+
+sum_product_network.compile(loss=loss, metrics=metrics, optimizer=optimizer)
+
+
+import tensorflow as tf 
+
+sum_product_network.fit(mnist_train, epochs=20, callbacks=[tf.keras.callbacks.ReduceLROnPlateau(monitor="loss", min_delta=0.1, patience=2, factor=0.5)])
+sum_product_network.evaluate(mnist_test)
+
+
+sum_product_network_sample = build_spn(spnk.SumOpSampleBackprop(), return_logits=False, infer_no_evidence=True)
+sum_product_network_sample.set_weights(sum_product_network.get_weights())
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+fig = plt.figure(figsize=(12., 12.))
+grid = ImageGrid(
+    fig, 111,
+    nrows_ncols=(10, 10),
+    axes_pad=0.1,
+)
+
+sample = sum_product_network_sample.zero_evidence_inference(100)
+
+print("Sampling done... Now ploting results")
+for ax, im in zip(grid, sample):
+    ax.imshow(np.squeeze(im), cmap="gray")
+plt.savefig("SPN_Sample.png")
+plt.show()
